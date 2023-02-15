c8c1df1e46acca5b152a6918f7bb3d7473585f6e
Terence <terencebc92@gmail.com>
2023-02-16 00:54:47 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import * as SDK from "azure-devops-extension-sdk";
+
+const App = () => {
+  useEffect(() => {
+    SDK.init({ loaded: false });
+    SDK.register("hot-reload", {
+      preload: () => {
+        return {
+          state: "ok",
+          statusType: 0,
+        };
+      },
+      load: () => {
+        return {
+          state: "ok",
+          statusType: 0,
+        };
+      },
+      reload: () => {
+        return {
+          state: "ok",
+          statusType: 0,
+        };
+      },
+    });
+    SDK.notifyLoadSucceeded();
+  }, []);
+  return <div>React template test</div>;
+};
+
+export default App;
